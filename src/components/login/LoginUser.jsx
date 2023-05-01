c0c093e1eeae6d266de9b347c3eb18dd177f9c00
Achmada Fiqri A Rasyad <38800157+fiqriachmada@users.noreply.github.com>
2023-05-01 18:29:40 +0700

    Update LoginUser.jsx

--- src/components/login/LoginUser.jsx
+++ src/components/login/LoginUser.jsx
@@ -24,7 +24,7 @@ function LoginPage({ setIsAuthenticated }) {
       })
       .catch((error) => {
         Swal.fire(error.message.toString(), "Gagal", "error");
-        setError(error.data.message.toString());
+        setError(error.response.data.toString());
       });
   };
 
