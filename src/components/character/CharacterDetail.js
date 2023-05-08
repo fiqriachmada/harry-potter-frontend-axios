d39c7a50d26aa57516c8a71194a60ae53ce2bf71
fiqriachmada <achmada1998@gmail.com>
2023-05-08 20:12:04 +0700

    new

--- src/components/character/CharacterDetail.js
+++ src/components/character/CharacterDetail.js
@@ -1,115 +1,137 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Col, Image, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { getCharacterById } from '../../apis/characterService'
-import './CharacterDetail.css'
+import React, { useEffect, useState } from 'react';
+import { Col, Image, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { getCharacterById } from '../../apis/characterService';
+import dotenv from 'dotenv';
+import './CharacterDetail.css';
+
+dotenv.config();
 
 const CharacterDetail = ({ match, history }) => {
-  var { id } = match.params
-  const [character, setCharacter] = useState([])
-
-  useEffect(() => {
-    getCharacterById(id).then(response => {
-      setCharacter(response.data.data)
-      console.log('response', response)
-    })
-  }, [id])
+  var { id } = match.params;
+  const [character, setCharacter] = useState([]);
 
   const handlePreviousClick = () => {
-    history.push(`/characters/${id - 1}`)
-  }
-  const handleNextClick = async () => {}
+    history.push(`/characters/${id - 1}`);
+  };
+  const handleNextClick = async () => {};
+
+  const urlImage = process.env.IMAGE_URL;
+
+  console.log(`process.env.URL_IMAGE`, `${process.env.URL_IMAGE}`);
+
+  console.log('urlImage', urlImage);
+
+  console.log(
+    'urlImage + / + character.image',
+    'https://ik.imagekit.io/fiqriachmada' + '/' + character.image
+  );
+
+  useEffect(() => {
+    getCharacterById(id).then((response) => {
+      setCharacter(response.data.data);
+    });
+  }, [id]);
 
   return (
     <Row>
-      <h3 className='mb-3'>Character Detail </h3>
-      <Col className='mt-3 mb-3'>
-        <div className='card shadow-lg py-5 mb-5 '>
-          <div className='d-flex justify-content-center mt-2 mb-2'>
+      <h3 className="mb-3">Character Detail </h3>
+      <Col className="mt-3 mb-3">
+        <div className="card shadow-lg py-5 mb-5 ">
+          <div className="d-flex justify-content-center mt-2 mb-2">
             <Image
               src={character.image}
               style={{
                 width: 250,
                 height: 250,
                 background: `lightgrey`,
-                borderRadius: 5
+                borderRadius: 5,
+              }}
+            />
+            <Image
+              src={
+                'https://ik.imagekit.io/fiqriachmada' + '/' + character.image
+              }
+              style={{
+                width: 250,
+                height: 250,
+                background: `lightgrey`,
+                borderRadius: 5,
               }}
             />
           </div>
-          <div className='card-body'>
-            <div className='col'>
-              <div className='col-md-12'>
-                <div className='row no-gutters align-items-center'>
-                  <div className='col mr-2'>
+          <div className="card-body">
+            <div className="col">
+              <div className="col-md-12">
+                <div className="row no-gutters align-items-center">
+                  <div className="col mr-2">
                     <h1>{character.full_name}</h1>
                   </div>
-                  <div className='col-auto'>
-                    <div className='btn-group float-right'>
+                  <div className="col-auto">
+                    <div className="btn-group float-right">
                       <Link
                         to={`/characters/edit/${character.id}`}
-                        className='btn btn-sm btn-primary mb-3'
-                      >
+                        className="btn btn-sm btn-primary mb-3">
                         Edit
                       </Link>
                     </div>
                   </div>
                 </div>
                 <hr />
-                <div className='row'>
-                  <div className='col-md-6'>
-                    <p className='card-text text-justify'>
+                <div className="row">
+                  <div className="col-md-6">
+                    <p className="card-text text-justify">
                       <strong>Deskripsi</strong> <br />
                       <br />
                     </p>
                   </div>
-                  <div className='col-md-6'>
+                  <div className="col-md-6">
                     <strong>Detail</strong> <br />
                     <br />
-                    <div className='row no-gutters align-items-center'>
-                      <div className='col mr-2'>
-                        <div className='mb-0 font-weight-bold text-gray-800'>
+                    <div className="row no-gutters align-items-center">
+                      <div className="col mr-2">
+                        <div className="mb-0 font-weight-bold text-gray-800">
                           Species
                         </div>
                       </div>
-                      <div className='col-auto'>
-                        <div className='btn-group float-right text-capitalize'>
+                      <div className="col-auto">
+                        <div className="btn-group float-right text-capitalize">
                           {character.species}
                         </div>
                       </div>
                     </div>
-                    <div className='row no-gutters align-items-center'>
-                      <div className='col mr-2'>
-                        <div className='mb-0 font-weight-bold text-gray-800'>
+                    <div className="row no-gutters align-items-center">
+                      <div className="col mr-2">
+                        <div className="mb-0 font-weight-bold text-gray-800">
                           Gender
                         </div>
                       </div>
-                      <div className='col-auto'>
-                        <div className='btn-group float-right text-capitalize'>
+                      <div className="col-auto">
+                        <div className="btn-group float-right text-capitalize">
                           {character.gender}
                         </div>
                       </div>
                     </div>
-                    <div className='row no-gutters align-items-center'>
-                      <div className='col mr-2'>
-                        <div className='mb-0 font-weight-bold text-gray-800'>
+                    <div className="row no-gutters align-items-center">
+                      <div className="col mr-2">
+                        <div className="mb-0 font-weight-bold text-gray-800">
                           House
                         </div>
                       </div>
-                      <div className='col-auto'>
-                        <div className='btn-group float-right text-capitalize'>
+                      <div className="col-auto">
+                        <div className="btn-group float-right text-capitalize">
                           {character.house || '-'}
                         </div>
                       </div>
                     </div>
-                    <div className='row no-gutters align-items-center'>
-                      <div className='col mr-2'>
-                        <div className='mb-0 font-weight-bold text-gray-800'>
+                    <div className="row no-gutters align-items-center">
+                      <div className="col mr-2">
+                        <div className="mb-0 font-weight-bold text-gray-800">
                           Date of Birth
                         </div>
                       </div>
-                      <div className='col-auto'>
-                        <div className='btn-group float-right text-capitalize'>
+                      <div className="col-auto">
+                        <div className="btn-group float-right text-capitalize">
                           {character.date_of_birth || '-'}
                         </div>
                       </div>
@@ -125,25 +147,23 @@ const CharacterDetail = ({ match, history }) => {
             id > 1
               ? 'd-flex justify-content-between mt-2'
               : 'd-flex justify-content-end mt-2'
-          }
-        >
+          }>
           {id > 1 && (
             <button
               disabled={id <= 1}
-              type='button'
-              class='btn btn-dark'
-              onClick={handlePreviousClick}
-            >
+              type="button"
+              class="btn btn-dark"
+              onClick={handlePreviousClick}>
               &larr; Previous
             </button>
           )}
-          <button type='button' class='btn btn-dark' onClick={handleNextClick}>
+          <button type="button" class="btn btn-dark" onClick={handleNextClick}>
             Next &rarr;
           </button>
         </div>
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default CharacterDetail
+export default CharacterDetail;
--- src/components/login/LoginUser.jsx
+++ src/components/login/LoginUser.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { loginUser } from "../../apis/userServices";
-import Swal from "sweetalert2";
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { loginUser } from '../../apis/userServices';
+import Swal from 'sweetalert2';
+import { Tabs, Tab, Alert } from 'react-bootstrap';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function LoginPage({ setIsAuthenticated }) {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const [error, setError] = useState('');
   const history = useHistory();
 
   const handleSubmit = async (event) => {
@@ -15,53 +24,134 @@ function LoginPage({ setIsAuthenticated }) {
     return loginUser({ username, email, password })
       .then((response) => {
         Swal.fire(
-          "Successfully Login with " + (email || username),
-          "",
-          "success"
+          'Successfully Login with ' + (email || username),
+          '',
+          'success'
         );
         setIsAuthenticated(true);
-        history.push("/characters/");
+        history.push('/characters/');
       })
       .catch((error) => {
-        Swal.fire(error.response.data.message, "Failed", "error");
+        Swal.fire(error.response.data.message, 'Failed', 'error');
         setError(error.response.data.message);
       });
   };
 
   return (
-    <div className="card" style={{ backgroundColor: "white" }}>
-      <div>
-        <form onSubmit={handleSubmit}>
-          {error && <p>{error}</p>}
-          <div className="row mt-5 mb-5 mx-1">
-            <label>
-              Username or Email:
-              <input
-                className="form-control"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </label>
+    <div className="card" style={{ backgroundColor: 'white' }}>
+      <Tabs
+        defaultActiveKey="username"
+        id="login-tabs"
+        className="justify-content-center">
+        <Tab
+          eventKey="username"
+          title="Username"
+          onClick={(e) => {
+            setEmail('');
+          }}>
+          <div className="container">
+            <form onSubmit={handleSubmit}>
+              {error && (
+                <Alert variant={'danger'} className="mt-3 mx-3">
+                  {error && <p>{error}</p>}
+                </Alert>
+              )}
+              <div className="row mt-5 mb-5 mx-1">
+                <label>
+                  Username:
+                  <input
+                    className="form-control"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="row mt-5 mb-5 mx-1">
+                <label>Password:</label>
+                <div className="input-group mb-3">
+                  <input
+                    value={password}
+                    type={showPassword ? 'text' : 'password'}
+                    className="form-control"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div className="input-group-append">
+                    <span className="input-group-text mx-auto my-auto">
+                      <FontAwesomeIcon
+                        className="mx-auto my-1"
+                        icon={showPassword ? faEyeSlash : faEye}
+                        onClick={toggleShowPassword}
+                      />
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-5 mb-5 mx-3">
+                <button className="btn btn-outline-primary" type="submit">
+                  Log In
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="row mt-5 mb-5 mx-1">
-            <label>
-              Password:
-              <input
-                className="form-control"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
+        </Tab>
+        <Tab
+          eventKey="email"
+          title="Email"
+          onClick={(e) => {
+            setUsername('');
+          }}>
+          <div>
+            <form onSubmit={handleSubmit}>
+              {error && (
+                <Alert variant={'danger'} className="mt-3 mx-3">
+                  {error && <p>{error}</p>}
+                </Alert>
+              )}
+              <div className="row mt-5 mb-5 mx-1">
+                <label>
+                  Email:
+                  <input
+                    className="form-control"
+                    type="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      setUsername('');
+                    }}
+                  />
+                </label>
+              </div>
+
+              <div className="row mt-5 mb-5 mx-1">
+                <label>Password:</label>
+                <div className="input-group mb-3">
+                  <input
+                    value={password}
+                    type={showPassword ? 'text' : 'password'}
+                    className="form-control"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div className="input-group-append">
+                    <span className="input-group-text mx-auto my-auto">
+                      <FontAwesomeIcon
+                        className="mx-auto my-1"
+                        icon={showPassword ? faEyeSlash : faEye}
+                        onClick={toggleShowPassword}
+                      />
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-5 mb-5 mx-3">
+                <button className="btn btn-outline-primary" type="submit">
+                  Log In
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="row mt-5 mb-5 mx-3">
-            <button className="btn btn-outline-primary" type="submit">
-              Log In
-            </button>
-          </div>
-        </form>
-      </div>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
