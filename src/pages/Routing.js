d5fea1f85ce9665997a51a25d31df7cac410e36b
fiqriachmada <achmada1998@gmail.com>
2023-06-06 14:10:36 +0700

    new, delete console

--- src/pages/Routing.js
+++ src/pages/Routing.js
@@ -1,29 +1,27 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Home from './Home'
-import Footer from './templates/Footer'
+import Home from './Home';
+import Footer from './templates/Footer';
 
-import CharacterList from '../components/character/CharacterList'
-import CharacterDetail from '../components/character/CharacterDetail'
-import CharacterEdit from '../components/character/CharacterEdit'
-import ProtectedRoute from './ProtectedRoute'
-import CharacterAdd from '../components/character/CharacterAdd'
-import { Redirect } from 'react-router-dom/cjs/react-router-dom'
-import LoginUser from '../components/user/UserLogin'
-import RegisterUser from '../components/user/UserRegister'
-import Navigation from './templates/Navigation.jsx'
-import ProfileUser from '../components/user/UserProfile'
-import UserEdit from '../components/user/UserEdit'
+import CharacterList from '../components/character/CharacterList';
+import CharacterDetail from '../components/character/CharacterDetail';
+import CharacterEdit from '../components/character/CharacterEdit';
+import ProtectedRoute from './ProtectedRoute';
+import CharacterAdd from '../components/character/CharacterAdd';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom';
+import LoginUser from '../components/user/UserLogin';
+import RegisterUser from '../components/user/UserRegister';
+import Navigation from './templates/Navigation.jsx';
+import ProfileUser from '../components/user/UserProfile';
+import UserEdit from '../components/user/UserEdit';
 
 const Routing = ({
   isAuthenticated,
   setIsAuthenticated,
   userProfile,
-  setUserProfile
+  setUserProfile,
 }) => {
-  console.log('userProfile', userProfile)
-  console.log('isAuthenticated', isAuthenticated)
   return (
     <BrowserRouter>
       <Navigation
@@ -31,11 +29,11 @@ const Routing = ({
         isAuthenticated={isAuthenticated}
         setIsAuthenticated={setIsAuthenticated}
       />
-      <section className='py-5 container mt-5'>
+      <section className="py-5 container mt-5">
         <Switch>
-          <Route path='/login'>
+          <Route path="/login">
             {isAuthenticated ? (
-              <Redirect to='/' />
+              <Redirect to="/" />
             ) : (
               <LoginUser
                 setIsAuthenticated={setIsAuthenticated}
@@ -43,28 +41,28 @@ const Routing = ({
               />
             )}
           </Route>
-          <Route path='/' exact component={Home} />
-          <Route path='/register' exact component={RegisterUser} />
+          <Route path="/" exact component={Home} />
+          <Route path="/register" exact component={RegisterUser} />
           <ProtectedRoute
-            path='/characters/add'
+            path="/characters/add"
             exact
             component={CharacterAdd}
             isAuthenticated={isAuthenticated}
           />
           <ProtectedRoute
-            path='/characters'
+            path="/characters"
             exact
             component={CharacterList}
             isAuthenticated={isAuthenticated}
           />
           <ProtectedRoute
-            path='/characters/edit/:id'
+            path="/characters/edit/:id"
             exact
             component={CharacterEdit}
             isAuthenticated={isAuthenticated}
           />
           <ProtectedRoute
-            path='/characters/:id'
+            path="/characters/:id"
             exact
             component={CharacterDetail}
             isAuthenticated={isAuthenticated}
@@ -102,7 +100,7 @@ const Routing = ({
       </section>
       <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Routing
+export default Routing;
